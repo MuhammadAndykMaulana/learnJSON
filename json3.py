7c2eb19a54f69f6b1cb3893582059394f3fe53cd
Muhammad Andyk Maulana (Brilliant Ace) <muhammadandykmaulana@gmail.com>
2018-05-17 12:52:59 +0700

    Initial Commit

---
+++ json3.py
@@ -0,0 +1,13 @@
+import json
+
+class User(object):
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
+alice = User('Alice A. Adams', 'secret')
+
+def jdefault(o):
+    if isinstance(o, set):
+       return list(o)
+    return o.__dict__
+print(json.dumps(alice, default=jdefault))
---
+++ json4.py
@@ -0,0 +1,14 @@
+class Student:
+ 
+      def __init__(self,name,email,contact,skills,ug=None,pg=None):
+ 
+          self.email=email
+          self.contact=contact
+          self.name=name
+          self.skills=[skills]
+ 
+          self.edu={"ug":[ug],"pg":[pg]} 
+
+james=Student("James","j@j.com","+1 7789990007","Python","CS", "CS")
+ 
+print vars(james)
